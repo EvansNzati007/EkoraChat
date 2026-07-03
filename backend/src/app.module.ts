e0ea19cee661e6ff56bae63cd76f042a803b111a
EvansNzati007 <nzatievans02@gmail.com>
2026-07-03 20:27:03 +0100

    chore(backend): init NestJS + Prisma + PostgreSQL

---
+++ backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConversationsModule } from './conversations/conversations.module';
+import { MessagesModule } from './messages/messages.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, ConversationsModule, MessagesModule, PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
